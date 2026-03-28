188b27cadcde3f386661e8d333f29b54c32c2372
SidSam15 <siddhant.samar@gmail.com>
2026-03-28 01:10:29 -0500

    intro

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Siddhant Samar" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("My name is Siddhant, a junior from Texas, and I really look forward to this class because of how much of a role computer science plays in my hobbies. I really enjoy building hardware and making things like robots (I do FTC and FRC!), and I love every step of creating them, from CAD to putting it together. Obviously computer science knowledge is vital to coding these projects of mine, so I am super excited for this course! Something interesting about me is how varied yet extensive my interests are; for example, I started my own FTC team from scratch from my neighbors garage since there were no other opportunities after my old team disbanded (Not a lot of FIRST near where I live, but I loved building robots too much to just leave), but I also really like archery (Going to nationals for NASP!) and cooking. Once again, I am really looking forward to this course (and RACECAR, which is what I am taking this course for) and cannot wait!")
 
 if __name__ == "__main__":
     main()
